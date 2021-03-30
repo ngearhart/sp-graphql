43baedcb892b873a56ac240efcceb648471d8075
Noah Gearhart <excelsiorvfx@gmail.com>
2021-03-29 21:09:56 -0400

    Clean up main.py, correct pip imports in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,20 @@ FROM python:3.9
 
 # Copy local files
 WORKDIR /sp-gql
-COPY . .
 
 # Expand datasets
+COPY SPEGQL-dataset.zip SPEGQL-dataset.zip 
+COPY cosql_dataset.zip cosql_dataset.zip 
+COPY spider.zip spider.zip 
 RUN unzip SPEGQL-dataset.zip \
 	&& unzip cosql_dataset.zip \
 	&& unzip spider.zip
 
 # Python dependencies
-RUN pip install jsonschema pytorch-lightning \
-	&& pip install install git+https://github.com/acarrera94/text-to-graphql-validation --log ./logs.txt
-
 RUN git clone https://github.com/huggingface/transformers && \
 	pip install ./transformers
+
+RUN pip install jsonschema pytorch-lightning pandas nltk numpy sentencepiece \
+	&& pip install install git+https://github.com/acarrera94/text-to-graphql-validation --log ./logs.txt
+
+COPY . .
--- main.py
+++ main.py
@@ -3,32 +3,111 @@ from model import T5MultiSPModel
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning import Trainer
 
-hparams = argparse.Namespace(**{'lr': 0.0004365158322401656}) # for 3 epochs
-# system = ConvBartSystem(dataset, train_sampler, batch_size=2)
-system = T5MultiSPModel(hparams,batch_size=32)
-# system.lr = 3e-4
+def pre_train(system: T5MultiSPModel) -> Trainer:
+    # trainer = Trainer(num_tpu_cores=8,max_epochs=1)   
+    # trainer = Trainer(max_epochs=1, train_percent_check=0.1)
+    # checkpoint_callback = ModelCheckpoint(
+    #     filepath=os.getcwd()+'/checkpoint',
+    #     verbose=True,
+    #     monitor='val_loss',
+    #     mode='min',
+    #     prefix=''
+    # )
+    # trainer = Trainer(gpus=1, max_epochs=1, progress_bar_refresh_rate=1)
 
-# trainer = Trainer(num_tpu_cores=8,max_epochs=1)   
-# trainer = Trainer(max_epochs=1, train_percent_check=0.1)
-# checkpoint_callback = ModelCheckpoint(
-#     filepath=os.getcwd()+'/checkpoint',
-#     verbose=True,
-#     monitor='val_loss',
-#     mode='min',
-#     prefix=''
-# )
-# trainer = Trainer(gpus=1, max_epochs=1, progress_bar_refresh_rate=1)
-trainer = Trainer(gpus=1, max_epochs=1, progress_bar_refresh_rate=1, train_percent_check=0.2)
-# trainer = Trainer(gpus=1, max_epochs=3, auto_lr_find=True, progress_bar_refresh_rate=1, train_percent_check=0.2)\
-# trainer = Trainer(gpus=1,max_epochs=1, progress_bar_refresh_rate=1, train_percent_check=0.2)
-# trainer = Trainer(gpus=1,max_epochs=1, progress_bar_refresh_rate=1, train_percent_check=0.2,checkpoint_callback=checkpoint_callback)
-# trainer = Trainer(num_tpu_cores=8,max_epochs=1, progress_bar_refresh_rate=1)
-# import gc
-# gc.collect()
+    # TODO: Is train percent check the same as val check interval?
+    trainer = Trainer(gpus=1, max_epochs=1, progress_bar_refresh_rate=1, val_check_interval=0.2)
+    # trainer = Trainer(gpus=1, max_epochs=3, auto_lr_find=True, progress_bar_refresh_rate=1, train_percent_check=0.2)\
+    # trainer = Trainer(gpus=1,max_epochs=1, progress_bar_refresh_rate=1, train_percent_check=0.2)
+    # trainer = Trainer(gpus=1,max_epochs=1, progress_bar_refresh_rate=1, train_percent_check=0.2,checkpoint_callback=checkpoint_callback)
+    # trainer = Trainer(num_tpu_cores=8,max_epochs=1, progress_bar_refresh_rate=1)
+    # import gc
+    # gc.collect()
 
-trainer.fit(system)
-system.tokenizer.decode(system.train_dataset[0]['source_ids'].squeeze(), skip_special_tokens=False, clean_up_tokenization_spaces=False)
-TXT = "query { faculty_aggregate { aggregate { <mask> } } } </s>"
-input_ids = system.tokenizer.batch_encode_plus([TXT], return_tensors='pt')['input_ids']
-# logits = system.model(input_ids)[0]
-system.tokenizer.decode(system.model.generate(input_ids.cuda())[0])
+    trainer.fit(system)
+    system.tokenizer.decode(system.train_dataset[0]['source_ids'].squeeze(), skip_special_tokens=False, clean_up_tokenization_spaces=False)
+    TXT = "query { faculty_aggregate { aggregate { <mask> } } } </s>"
+    input_ids = system.tokenizer.batch_encode_plus([TXT], return_tensors='pt')['input_ids']
+    # logits = system.model(input_ids)[0]
+    system.tokenizer.decode(system.model.generate(input_ids.cuda())[0])
+    return trainer
+
+def fine_tune(system: T5MultiSPModel) -> Trainer:
+    system.task = 'finetune'
+    system.batch_size = 2 # because t5-base is smaller than bart.
+    # system.lr=3e-4 # -6 is original
+    # system.batch_size = 16
+    system.hparams.lr=0.0005248074602497723 # same as 5e-4
+    # system.hparams.lr=3e-4
+    # TODO collate to go back to 16
+    # system.model.config.output_past=True
+    # system.model.model.decoder.output_past=True
+    system.prepare_data() # might not be needed. 
+    # system.add_special_tokens()
+    # system.model.output_past = True
+    # trainer = Trainer(num_tpu_cores=8,max_epochs=1)   
+    # trainer = Trainer(max_epochs=1, train_percent_check=0.1)
+    # checkpoint_callback = ModelCheckpoint(
+    #     filepath=os.getcwd()+'/checkpoint_finetuning',
+    #     verbose=True,
+    #     monitor='val_loss',
+    #     mode='min',
+    #     prefix=''
+    # )
+
+    # trainer = Trainer(gpus=1,max_epochs=1, progress_bar_refresh_rate=1, train_percent_check=0.2)
+    # trainer = Trainer(gpus=1, progress_bar_refresh_rate=1, val_check_interval=0.4)
+    trainer = Trainer(gpus=1, max_epochs=5, progress_bar_refresh_rate=1, val_check_interval=0.5)
+    # trainer = Trainer(gpus=1, max_epochs=3, progress_bar_refresh_rate=1, val_check_interval=0.5)
+    # trainer = Trainer(gpus=1,max_epochs=3, progress_bar_refresh_rate=1,checkpoint_callback=checkpoint_callback)
+    # trainer = Trainer(num_tpu_cores=8,max_epochs=1, progress_bar_refresh_rate=1)
+    trainer.fit(system)
+    inputs = system.val_dataset[0]
+    system.tokenizer.decode(inputs['source_ids'])
+    # system.tokenizer.decode(inputs['target_ids'])
+
+    # inputs = system.tokenizer.batch_encode_plus([user_input], max_length=1024, return_tensors='pt')
+    # generated_ids = system.bart.generate(example['input_ids'].cuda(), attention_mask=example['attention_mask'].cuda(), num_beams=5, max_length=40,repetition_penalty=3.0)
+    # maybe i didn't need attention_mask? or the padding was breaking something.
+    # attention mask is only needed  
+    generated_ids = system.model.generate(inputs['source_ids'].unsqueeze(0).cuda(), num_beams=5, repetition_penalty=1.0, max_length=56, early_stopping=True)
+    # summary_text = system.tokenizer.decode(generated_ids[0])
+
+    hyps = [system.tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in generated_ids]
+    print(hyps)
+    # trainer.save_checkpoint('finished.ckpt')
+    # !zip -r finished_train.zip finished.ckpt
+    system = system.load_from_checkpoint('finished.ckpt')
+    system.task='finetune'
+    trainer = Trainer(gpus=1, max_epochs=0, progress_bar_refresh_rate=1, val_check_interval=0.5)
+    trainer.fit(system)
+    return trainer
+
+def test(system: T5MultiSPModel, trainer: Trainer, test_flag: str):
+    system.num_beams = 3
+    system.test_flag = test_flag
+    system.prepare_data()
+    trainer.test()
+
+
+def main():
+    hparams = argparse.Namespace(**{'lr': 0.0004365158322401656}) # for 3 epochs
+    # system = ConvBartSystem(dataset, train_sampler, batch_size=2)
+
+    print("Creating model...")
+    system = T5MultiSPModel(hparams,batch_size=32)
+    # system.lr = 3e-4
+
+    print("Pretraining...")
+    pre_train(system)
+
+    print("Fine tuning...")
+    trainer = fine_tune(system)
+    print("Testing...")
+    test(system, trainer, 'graphql')
+    test(system, trainer, 'sql')
+
+    # TODO: NTLK and Flask stuff
+
+if __name__ == "__main__":
+    main()
